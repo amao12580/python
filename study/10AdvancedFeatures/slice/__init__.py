f5a75b7daf95bc5bfccc691315db6fd3ee2fb4a9
成亮 <chengl@zhengtongauto.net>
2019-03-11 17:29:12 +0800

    学习内容：高级特性-切片（slice）

---
+++ study/10AdvancedFeatures/slice/__init__.py
@@ -0,0 +1,19 @@
+l = list(range(1, 10))
+
+print(l)
+head = l[0:3]
+print(head)
+print(l[0])
+print(l[3])
+l.insert(0, 0)
+print(l)
+
+t = range(5, 20)
+head = t[0:3]
+print(head)
+print(t[0])
+print(t[3])
+
+print(head[0])
+print(head[2])
+print(t)
