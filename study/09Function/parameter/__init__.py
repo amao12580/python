23447d270f37160a6e8a09e6be091a073de14307
成亮 <chengl@zhengtongauto.net>
2019-03-11 17:13:35 +0800

    学习内容：函数参数：必选参数、默认参数、可变参数、关键字参数

--- study/09Function/parameter/__init__.py
+++ study/09Function/parameter/__init__.py
@@ -8,6 +8,7 @@
 
 # 关键字参数，可变参数允许你传入0个或任意个参数，这些可变参数在函数调用时自动组装为一个tuple。而关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict。
 
+
 def power(x: float, n: int = 2):
     if n <= 0:
         raise ValueError('n must greater than zero.')
