1546cbac309c61194cb9a44c0f640178231cd14a
成亮 <chengl@zhengtongauto.net>
2019-03-12 11:29:57 +0800

    学习内容：高级特性-生成器

--- study/10AdvancedFeatures/generator/__init__.py
+++ study/10AdvancedFeatures/generator/__init__.py
@@ -13,3 +13,46 @@ print(next(l2))
 print('----------------')
 for x in l2:
     print(x)
+
+
+def fib(n: int = 1):
+    r = []
+    left, right, c = 0, 1, 1
+    while c <= n:
+        print(right)
+        r.append(right)
+        left, right, c = right, left + right, c + 1
+    return r
+
+
+'''
+a, b = b, a + b
+相当于：
+
+t = (b, a + b) # t是一个tuple
+a = t[0]
+b = t[1]
+'''
+
+fib(10)
+
+
+# 如果一个函数定义中包含yield关键字，那么这个函数就不再是一个普通函数，而是一个generator：
+
+
+def fib_2(n: int = 1):
+    left, right, c = 0, 1, 1
+    while c <= n:
+        yield right
+        left, right, c = right, left + right, c + 1
+
+
+print(type(fib()))  # <class 'list'>
+
+print(type(fib_2()))  # <class 'generator'>
+
+for x in fib(12):
+    print(x)
+
+for x in fib_2(12):
+    print(x)
