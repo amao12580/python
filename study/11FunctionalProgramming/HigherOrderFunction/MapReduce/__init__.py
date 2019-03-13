530edd54824bec76a6bb207a36c8de525c156d40
成亮 <chengl@zhengtongauto.net>
2019-03-13 12:43:22 +0800

    学习内容：函数式编程-map/reduce

--- study/11FunctionalProgramming/HigherOrderFunction/MapReduce/__init__.py
+++ study/11FunctionalProgramming/HigherOrderFunction/MapReduce/__init__.py
@@ -59,3 +59,41 @@ if prod([3, 5, 7, 9]) == 945:
     print('测试成功!')
 else:
     print('测试失败!')
+
+
+# 利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+
+def str2float_v1(s: str):
+    return float(s)
+
+
+def str2int(s: str):
+    if not s.isdigit():
+        return 0
+    return int(s)
+
+
+def my_sum(x: float, y: float):
+    return x + y
+
+
+def str2float(s: str):
+    if s is None or s == '':
+        return None
+    i = s.index('.')
+    if i is None or i < 0:
+        i = len(s)
+    l = list(map(str2int, s))
+    for index, x in enumerate(l):
+        n = i - index
+        if index < i:
+            n = n - 1
+        l[index] = l[index] * pow(10, n)
+    return reduce(my_sum, l)
+
+
+print('str2float(\'123.456\') =', str2float('123.456'))
+if abs(str2float('123.456') - 123.456) < 0.00001:
+    print('测试成功!')
+else:
+    print('测试失败!')
