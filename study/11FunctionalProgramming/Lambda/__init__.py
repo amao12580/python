5cd8ef1de5ceadd654418adc967a6abbcbe636b7
成亮 <chengl@zhengtongauto.net>
2019-03-13 16:55:13 +0800

    学习内容：函数式编程-匿名函数

--- study/11FunctionalProgramming/Lambda/__init__.py
+++ study/11FunctionalProgramming/Lambda/__init__.py
@@ -3,11 +3,10 @@
 # -*- coding: utf-8 -*-
 
 
-def is_odd(x): return x % 2 == 1
-
+is_odd = lambda x: x % 2 == 1
 
 print(type(is_odd))
 print(is_odd)
-L = list(filter(is_odd, range(1, 20)))
+L = list(filter(lambda x: x % 2 == 1, range(1, 20)))
 
 print(L)
