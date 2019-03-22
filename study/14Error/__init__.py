ae5c3539de2b6a596b4ad0a59c9f8d5f67c2ffff
成亮 <chengl@zhengtongauto.net>
2019-03-22 20:04:00 +0800

    学习内容：错误处理与预防

--- study/14Error/__init__.py
+++ study/14Error/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import logging
+
 """
 -------------------------------------------------
    Project Name：  study
@@ -18,13 +20,31 @@
 
 __author__ = 'steven'
 
+# logging.basicConfig(filename='logs/%(levelname)s.log',level=logging.DEBUG,format='%(asctime)s %(filename)s[line:%(lineno)d] %(message)s',datefmt='%Y-%m-%d %X')
+
+# 2019-03-19 20:04:25.682  INFO [zhengtyun-system,95c06ff569dfdfd3,95c06ff569dfdfd3,true] 12056 --- [nio-8014-exec-6] com.zhengt.system.controller.TestController                            [stdIn] [139] : signature:com.zhengt.system.controller.TestController.login
+
+logging.basicConfig(
+    # filename='logs/%(levelname)s.log'
+    filename='test.log'
+    , level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s [%(filename)s] %(process)d [%(threadName)s] %(pathname)s [%('
+           'funcName)s]  [%(lineno)d] : %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.info('this is the info message')
+logging.debug('this is the debug message')
+logging.warning('this is the warning message')
+logging.error('this is the error message')
+logging.critical('this is the critical message')
+
 
 class BaseValueError(ValueError):
     pass
 
 
 def test(*args, **kwargs):
-    print(args, len(args), kwargs, len(kwargs))
+    logging.info(args)
     if len(args) >= 10:
         raise BaseValueError('args length must less than 10.%s' % str(args))
 
@@ -33,7 +53,8 @@ try:
     test(range(10))
     test(*range(10))
 except ValueError as e:
-    print('ValueError!', e.args)
+    # print('ValueError!', e.args)
+    logging.exception(e)
     raise
 # except BaseValueError as e:
 #     print('BaseValueError!', e.args)
